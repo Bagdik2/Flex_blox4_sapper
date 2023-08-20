d7db498b642c0ba2788a747dd1f54c945374cbe5
Bagdik2 <stup0903@gmail.com>
2023-08-21 01:47:28 +0300

    New commit after many years

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <windows.h>
 #include <gl/gl.h>
-
+//haaaii!
 LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
 void EnableOpenGL(HWND hwnd, HDC*, HGLRC*);
 void DisableOpenGL(HWND, HDC, HGLRC);
